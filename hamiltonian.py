bb4165855f68b496484a556f7c1cf7de3d8d51d3
Sunghyun Kim <kimsunghyun@kaist.ac.kr>
2016-05-15 02:04:04 +0900

    1) serious bug fixed: image_ind for zero-translation is self.system.structure.max_image/2 not 0 2) temporary triangular matrix formation for speed up is suspended

--- hamiltonian.py
+++ hamiltonian.py
@@ -1,7 +1,6 @@
 #/usr/local/bin/python
 import numpy as np
 from system import System, Structure, Lattice, Atom
-import system 
 from tb_params import HOPPING_INTEGRALS
 
 
@@ -37,7 +36,6 @@ class Hamiltonian(object):
 
         dist_mat_vec = self.system.structure.dist_mat_vec
         bond_mat = self.system.structure.bond_mat
-        # norm_factor = np.sum(bond_mat, axis=0)
 
         for ind_1, (atom_1_i, orbit_1_i, element_1, orbit_1) in enumerate(self.system.all_iter):
             for ind_2, (atom_2_i, orbit_2_i, element_2, orbit_2) in enumerate(self.system.all_iter):
@@ -47,9 +45,9 @@ class Hamiltonian(object):
                     dist_vec = dist_mat_vec[image_ind, atom_1_i, atom_2_i, :]
 
                     phase = np.exp(2*np.pi*1j * np.dot(kpt_cart, dist_vec))
-                    g_mat[image_ind, ind_1, ind_2] = phase #/ norm_factor[atom_1_i, atom_2_i]
-
-        g_mat[0, :, :] += np.eye(self.n_orbitals)
+                    g_mat[image_ind, ind_1, ind_2] = phase 
+        # non-translated image_ind is self.system.structure.max_image/2
+        g_mat[self.system.structure.max_image/2, :, :] += np.eye(self.n_orbitals)
         return g_mat
 
     def calc_ham_wo_k(self):
@@ -88,19 +86,20 @@ class Hamiltonian(object):
         for ind_1, (atom_1_i, orbit_1_i, element_1, orbit_1) in enumerate(self.system.all_iter):
             for ind_2, (atom_2_i, orbit_2_i, element_2, orbit_2) in enumerate(self.system.all_iter):
                 # only lower lower-triangular part
-                if ind_2 >= ind_1:
-                    continue
+                # problem occurs ... | atom 1 | atom 1 | atom 1| ...
+                # if ind_1 > ind_2:
+                #     continue
 
                 hop_int = HOPPING_INTEGRALS[Hamiltonian.get_orb_ind(orbit_1)][Hamiltonian.get_orb_ind(orbit_2)]
 
                 for image_ind in range(self.system.structure.max_image):
-                    # skip unbonded pairs
+                    # skip unbonding pairs
                     if bond_mat[image_ind, atom_1_i, atom_2_i] == 0:
                         continue
-
-                    param_element = params['{}{}'.format(element_1, element_2)]
+                    
                     param_element = self.system.get_params(atom_1_i, atom_2_i, image_ind)
                     hop_int_ele = hop_int.subs(param_element)
+                    
                     # get direction cosines
                     dist_vec = dist_mat_vec[image_ind, atom_1_i, atom_2_i, :]
                     l, m, n = get_dir_cos(dist_vec)
@@ -109,18 +108,18 @@ class Hamiltonian(object):
                     # sympy might be slow 
                     # May be just function is enough?
                     hop_int_ = hop_int_ele.subs(param_lmn)
-
                     self.H_wo_g[image_ind, ind_1, ind_2] = hop_int_.subs(param_element)
         
         # real hermitian -> symmetric
-        self.H_wo_g += np.transpose(self.H_wo_g, [0, 2, 1])
+        # self.H_wo_g += np.transpose(self.H_wo_g, [0, 2, 1])#[range(self.H_wo_g.shape[0])[::-1],:,:]
 
         # diagonal
         H_ind = 0
         for element, orbit in self.system.all_orbitals:
             e_ele = params[element]\
                           ['{}{}'.format(Hamiltonian.E_PREFIX, orbit[0])]
-            self.H_wo_g[0, H_ind, H_ind] = e_ele
+            # non-translated image_ind is self.system.structure.max_image/2
+            self.H_wo_g[self.system.structure.max_image/2, H_ind, H_ind] = e_ele
             H_ind += 1
 
 def get_kpt_path(sp_kpts, kpt_den=10):
@@ -157,40 +156,5 @@ def get_kpt_len(kpts, lat_mat):
 
     return kpts_len
 
-def main():
-    np.set_printoptions(precision=3)
-    fcc_ge_sys = system.fcc_ge_sys()
-    fcc_ge_ham = Hamiltonian(fcc_ge_sys)
-
-    kpts_1 = get_kpt_path([np.array([0., 0., 0]),
-                           np.array([0.0, 0.5, 0.5]),
-                           np.array([1/4., 5/8., 5/8.])], 100)
-    kpts_2 = get_kpt_path([np.array([3/8., 3/4., 3/8.]),
-                           np.array([0, 0, 0])], 100)
-    kpts = kpts_1 + kpts_2
-
-    kpts_len_1 = get_kpt_len(kpts_1, fcc_ge_sys.structure.lattice.get_matrix())
-    kpts_len_2 = get_kpt_len(kpts_2, fcc_ge_sys.structure.lattice.get_matrix())
-
-    kpts_len = np.append(kpts_len_1, kpts_len_2 + kpts_len_1[-1])
-
-    eigs_k = []
-
-    for kpt in kpts:
-        ham = fcc_ge_ham.get_ham(np.array(kpt))
-        eigs = np.linalg.eigvalsh(ham)
-        eigs_k.append(eigs)
-
-    # n_band = 8
-    eigs_k = np.array(eigs_k).T
-    
-    import matplotlib.pyplot as plt
-    for i, eigs in enumerate(eigs_k[:8]):
-        x = kpts_len
-        plt.plot(x, eigs, '-', color='#FF00FF')
-    plt.show()
-
-
-
 if __name__ == '__main__':
-    main()
+    pass
